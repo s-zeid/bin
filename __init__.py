f056a269935605b915b8531d5486151c213b8dba
Scott Zeid <s@zeid.me>
2013-12-15 11:02:34 -0600

    [__init__.py] argv-fixing wrapper around main() for Python scripts; absolute paths for module.__file__; some refactoring

--- __init__.py
+++ __init__.py
@@ -17,16 +17,28 @@ class ShellScript(object):
    argv = argv[0]
   return self.Popen(argv).wait()
  def Popen(self, argv, *popen_args, **popen_kwargs):
-  if not isinstance(argv, (list, tuple)) or len(argv) == 0:
-   argv = [None]
-  else:
-   argv = list(unicode(i).encode("utf-8") if not isinstance(i, basestring) else i
-               for i in argv[:])
-  if not isinstance(argv[0], basestring) or not argv[0]:
-   argv[0] = self.filename
+  argv = fix_argv(argv, self.filename)
   popen_kwargs = popen_kwargs.copy()
   popen_kwargs["executable"] = self.filename
   return subprocess.Popen(argv, *popen_args, **popen_kwargs)
+
+class MainWrapper(object):
+ __slots__ = ["filename", "original"]
+ def __init__(self, original, filename):
+  self.filename = filename
+  self.original = original
+ def __call__(self, argv=None, *args, **kwargs):
+  self.original(fix_argv(argv, self.filename), *args, **kwargs)
+
+def fix_argv(argv, filename):
+ if not isinstance(argv, (list, tuple)) or len(argv) == 0:
+  argv = [None]
+ else:
+  argv = list(unicode(i).encode("utf-8") if not isinstance(i, basestring) else i
+              for i in argv[:])
+ if not isinstance(argv[0], basestring) or not argv[0]:
+  argv[0] = filename
+ return argv
 
 def script(script, name=None):
  if name == None:
@@ -42,11 +54,19 @@ def script(script, name=None):
   sys.dont_write_bytecode = True
   module = imp.load_source(name, script)
   sys.dont_write_bytecode = dont_write_bytecode
+  module.__file__ = os.path.abspath(module.__file__)
+  if callable(getattr(module, "main", None)):
+   module.main = MainWrapper(module.main, module.__file__)
  else:
   module = types.ModuleType(name)
-  module.main = ShellScript(script)
-  module.__file__ = script
+  module.__file__ = os.path.abspath(script)
+  module.main = ShellScript(module.__file__)
+  sys.modules[name] = module
  return module
+
+def scripts():
+ return [i for i in sorted(os.listdir(ROOT))
+         if "." not in i and not os.path.isdir(os.path.join(ROOT, i))]
 
 def main(argv):
  if len(argv) < 2:
