43929371d0a0aa068c70d60c28744a7090d6db2f
Scott Zeid <s@zeid.me>
2013-12-15 11:17:06 -0600

    [__main__.py] add stub main method

--- __main__.py
+++ __main__.py
@@ -2,8 +2,11 @@ import sys
 
 import __init__
 
+def main(argv):
+ return __init__.main(argv)
+
 if __name__ == "__main__":
  try:
-  sys.exit(__init__.main(sys.argv))
+  sys.exit(main(sys.argv))
  except KeyboardInterrupt:
   pass
